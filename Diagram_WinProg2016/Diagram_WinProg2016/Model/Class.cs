731143880eacb056cdd828bf6bcb9ddbd8140d4e
thomasnilsson <s144470@student.dtu.dk>
2016-10-30 12:35:35 +0100

    Changes of class - mainViewmodel

--- Diagram_WinProg2016/Diagram_WinProg2016/Model/Class.cs
+++ Diagram_WinProg2016/Diagram_WinProg2016/Model/Class.cs
@@ -7,28 +7,40 @@ using System.ComponentModel;
 
 namespace Diagram_WinProg2016.Model
 {
-    class Class : NotifyBase
+    public class Class : NotifyBase
     {
-        private int X { get; set; }
-        private int Y { get; set; }
-        private int Height { get; set; }
-        private int Width { get; set; }
-        private int GridCenterX { get { return X + Width / 2; } set { X = value - Width / 2; NotifyPropertyChanged(() => X); } }
-        private int GridCenterY { get { return Y + Height / 2; } set { Y = value - Width / 2; NotifyPropertyChanged(() => Y); } }
-        private string ClassName { get; set; }
-        private List<string> Fields { get; set; }
-        private List<string> Methods { get; set; }
-        public double CenterX => Width / 2;
-        public double CenterY => Height / 2;
 
-        public Class()
+        private int x, y, number; // position and unique ID
+        public int Number { get { return number; } private set { number = value; } } //ID property
+        public int X { get { return x; } set { x = value; NotifyPropertyChanged("X"); } }
+        public int Y { get { return y; } set { y = value; NotifyPropertyChanged("Y"); } }
+        private int width, height;
+        public int Width { get { return width; } set { width = value; NotifyPropertyChanged("Width"); } }
+        public int Height { get { return height; } set { height = value; NotifyPropertyChanged("Height"); } }
+        public int CenterX { get { return X + Width / 2; } set { X = value - Width / 2; NotifyPropertyChanged("X"); } }
+        public int CenterY { get { return Y + Height / 2; } set { Y = value - Height / 2; NotifyPropertyChanged("Y"); } }
+
+        private string className;
+        public string ClassName { get { return className; } set { className = value; NotifyPropertyChanged("ClassName"); } }
+
+        private List<string> fields, methods;
+        public List<string> Fields { get { return fields; } set { fields = value; NotifyPropertyChanged("Fields"); } }
+        public List<string> Methods { get { return methods; } set { methods = value; NotifyPropertyChanged("Fields"); } }
+
+        //constructor
+        public Class(int newNum)
         {
-            ClassName = "My Class Name";
-            Fields[0] = "My Fields";
-            Methods[0] = "My Methods";
 
+            Number = newNum;
+            X = Y = 100; //start pos
+            Width = Height = 200; //initial size
+            className = "New Class"; //initial class name
+            fields = new List<string>();
+            methods = new List<string>();
+            fields.Add("First Field");
+            methods.Add("Second Method");
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        //public event PropertyChangedEventHandler PropertyChanged;
     }
 }
--- Diagram_WinProg2016/Diagram_WinProg2016/ViewModel/MainViewModel.cs
+++ Diagram_WinProg2016/Diagram_WinProg2016/ViewModel/MainViewModel.cs
@@ -1,24 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Diagram_WinProg2016.Commands
 using GalaSoft.MvvmLight;
-using GalaSoft.MvvmLight.CommandWpf;    
-using System.Text;
-using System.Threading.Tasks;
-
-
+using Diagram_WinProg2016.Model;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace Diagram_WinProg2016.ViewModel
 {
-    public class MainViewModel
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
     {
-        private UndoRedoControllerCmd undoRedoController = UndoRedoControllerCmd.Instance;
+        public ObservableCollection<Class> Classes{ get; set; }
 
-        public ICommand UndoCommand { get; }
-        public ICommand RedoCommand { get; }
+        public ICommand AddClassCommand { get; private set; }
+
+        public MainViewModel()
+        {
+            Classes = new ObservableCollection<Class>();
+            AddClassCommand = new RelayCommand(AddClassBox);
 
 
-
+        }
+        public void AddClassBox()
+        {
+            //undoRedoController.AddAndExecute(new AddClassCommand(ClassBoxs));
+        }
     }
 }
