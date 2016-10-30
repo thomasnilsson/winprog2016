ecb21d780f4944a1c687d92d014f58e766c5da27
Maria Lyck Carstensen <marialyckcarstensen@gmail.com>
2016-10-30 13:12:02 +0100

    Minor comments

--- Diagram_WinProg2016/Diagram_WinProg2016/Model/NotifyBase.cs
+++ Diagram_WinProg2016/Diagram_WinProg2016/Model/NotifyBase.cs
@@ -8,7 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 namespace Diagram_WinProg2016.Model
 {
-        public abstract class NotifyBase : INotifyPropertyChanged
+
+    //Dette er en abstrakt klasse som bliver brugt til at definere INotifyPropertyChanged funktionaliteten som bliver brugt af flere af modelklasserne. 
+    //Så det ikke skal defineres i hver klassefil.
+
+    //Ideen med INotifyPropertyChanged er at informere GUI delen om at egenskaben af et bundet element har ændret sig. Således den korresponderende grafik kan blive opdateret i henhold hertil!
+
+    public abstract class NotifyBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
