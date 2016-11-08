5a319b2b653d75ac988d59a9347afd159e670cb1
Mathias Enggrob Boon <Mathias Enggrob Boon>
2016-11-08 10:00:43 +0100

    Moved ViewModel files

---
+++ Diagram_WinProg2016/Diagram_WinProg2016/ViewModel/ClassViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using Diagram_WinProg2016.Model;
+using System.ComponentModel;
+
+namespace Diagram_WinProg2016.ViewModel
+{
+	class ClassViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected Class classObj { get; }
+
+		protected ClassViewModel(Class classObj) {
+			this.classObj = classObj;
+		}
+
+		public List<string> Data { get; set; }
+
+		public int Height
+		{
+			get { return classObj.Height; }
+			set
+			{
+				classObj.Height = value; OnPropertyChanged("Height");
+			}
+		}
+
+		public int Width
+		{
+			get { return classObj.Width; }
+			set
+			{
+				classObj.Width = value; OnPropertyChanged("Width");
+			}
+		}
+
+		public int X
+		{
+			get { return classObj.X; }
+			set
+			{
+				classObj.X = value;
+				OnPropertyChanged("X");
+			}
+		}
+		public int Y
+		{
+			get { return classObj.Y; }
+			set
+			{
+				classObj.Y = value;
+				OnPropertyChanged("Y");
+			}
+		}
+		public string className {
+			get { return classObj.ClassName; }
+		}
+		public override string ToString() => classObj.ToString();
+
+		protected void OnPropertyChanged(string name)
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null)
+			{
+				handler(this, new PropertyChangedEventArgs(name));
+			}
+		}
+	}
+
+}
---
+++ Diagram_WinProg2016/Diagram_WinProg2016/ViewModel/ConnectorViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagram_WinProg2016.ViewModel
+{
+	class ConnectorViewModel
+	{
+
+		public ClassViewModel startClass {
+
+		}
+
+		public ClassViewModel endClass {
+
+		}
+	}
+}
